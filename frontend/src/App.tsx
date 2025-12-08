124e20c75234468cc152cacc2bf0dae0f669ea05
lumineon213 <cht3109@naver.com>
2025-12-08 10:08:41 +0900

    공지사항 및 약관 페이지 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,11 @@ import FoodDetail from "pages/foodList/foodDetail";
 
 import CourseHome from "pages/course/CourseHome";
 import WalkCourseList from "pages/course/WalkCourseList";
+import PrivacyPolicy from "pages/footer_details/PrivacyPolicy";
+import Terms from "pages/footer_details/Terms";
+import NoticeList from "pages/notice/NoticeList";
+import NoticeDetail from "pages/notice/NoticeDetail";
+import NoticeWrite from "pages/notice/NoticeWrite";
 
 // 타입 정의
 export interface SolrResultItem {
@@ -29,22 +34,30 @@ function App() {
           <>
           <Header />
                 <Routes>                
-                  //새로운 메인 페이지
+                  {/* 새로운 메인 페이지 */}
                   <Route path="/" element={<MainPage/>} />                   
-                  //로그인
+                  {/* 로그인 */}
                   <Route path="/login" element={<Login />} />
-                  //마이페이지
+                  {/* 마이페이지 */}
                    <Route path="/mypage" element={<MyPage />} />
-                  //맛집 리스트
+                  {/* 맛집 리스트 */}
                   <Route path="/food" element={<FoodList />} />
-                  //지도 페이지
+                  {/* 지도 페이지 */}
                    <Route path="/map" element={<MapPage />} />
-                  //상세보기
+                  {/* 상세보기 */}
                   <Route path="/food/:id" element={<FoodDetail />} />
-                  //여행 코스
+                  {/* 여행 코스 */}
                   <Route path="/course" element={<CourseHome />} />
-                  //도보여행
+                  {/* 도보여행 */}
                   <Route path="/course/walk" element={<WalkCourseList />} />
+                  {/* 공지사항 */}
+                  <Route path="/notice" element={<NoticeList />} />
+                  <Route path="/notice/:id" element={<NoticeDetail />} />
+                  <Route path="/notice/write" element={<NoticeWrite />} />
+                  {/* 개인정보 처리방침 */}
+                  <Route path="/footer_details/privacy" element={<PrivacyPolicy />} />
+                  {/* 이용약관 */}
+                  <Route path="/footer_details/terms" element={<Terms />} />
                 </Routes>
          
           <Footer />
