e922411b6ea81b54b1b4dfb37ff9d6f535bb1c64
lumineon213 <cht3109@naver.com>
2025-12-10 08:22:04 +0900

    Revert "Sociallogin"

--- src/main/java/com/boot/service/loginService.java
+++ src/main/java/com/boot/service/loginService.java
@@ -9,52 +9,6 @@ public interface loginService {
     boolean emailCheck(String email);
 
     boolean phoneCheck(String phoneNumber);
-    
-    // 아이디 중복 체크 (신규 추가)
-    boolean accountIdCheck(String accountId);
 
     loginDTO login(String accountId, String accountPw);
-
-    loginDTO findOrCreateSocialUser(String type, String socialId, String email, String name);
-
-
-    // 아이디 비밀번호 찾기
-
-    // 아이디 찾기
-    String findId(String email, String phoneNumber);
-
-    // 유저 존재 여부 (비밀번호 찾기 시 사용)
-    boolean existUser(String accountId, String email);
-
-    // 임시 비밀번호 생성
-    String createTempPw();
-
-    // 비밀번호 변경
-    void updatePassword(String accountId, String tempPw);
-
-    // 임시 비밀번호 이메일 발송
-    void sendTempPasswordMail(String email, String tempPw);
-
-
-
-    //  Token 기반 비밀번호 변경
-    String generateResetToken(String accountId);
-
-    loginDTO getUserByResetToken(String token);
-
-    boolean updatePasswordUsingToken(String token, String newPw);
-
-    void sendPasswordResetMail(String email, String link);
-
-
-    // 이메일 인증 번호 생성
-    String generateEmailVerifyCode(String email);
-
-    // 인증번호 이메일 발송
-    void sendEmailVerifyMail(String email, String code);
-
-    // 인증번호 검증
-    boolean verifyEmailCode(String email, String code);
-
-
 }
