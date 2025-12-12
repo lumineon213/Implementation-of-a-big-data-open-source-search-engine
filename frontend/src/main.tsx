936b56e60bac4bf096d2ab1ad161a9a8cacad450
손봉균 <sbg0629@naver.com>
2025-12-12 12:18:25 +0900

    dark
    이상 한 부분 있으면 말해줘요

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-
+import { DarkModeProvider } from './contexts/DarkModeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   //<StrictMode>
     <BrowserRouter>
-      <App />
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
     </BrowserRouter>
   //</StrictMode>,
 )
